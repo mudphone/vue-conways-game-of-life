902f9b8d93d5798c25394e7f559ff28f375b7828
Kyle Oba <kyle@fathom.info>
2022-08-20 22:57:31 -1000

    Add GOL rules and text representation

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,109 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue'
+  import * as _ from 'lodash'
+
+  const D = 0 // dead
+  const A = 1 // alive
+
+  const ROWS = 20
+  const COLS = 20
+
+  const getInitState = () => {
+    const s = []
+    _.forEach(_.range(ROWS), () => {
+      s.push(_.fill(Array(COLS), D))
+    })
+    _.forEach(_.range(ROWS), r => {
+      _.forEach(_.range(COLS), c => {
+        if (_.random(10) >= 5) {
+          s[r][c] = A
+        }
+      })
+    })
+    return s
+  }
+
+  const initialState = getInitState()
+  const gameState = ref(initialState)
+
+  const iterateGame = () => {
+    updateState(_.cloneDeep(gameState.value))
+  }
+
+  const updateState = (state) => {
+    console.debug('old state', state)
+    _.forEach(_.range(ROWS), r => {
+      _.forEach(_.range(COLS), c => {
+        gameState.value[r][c] = cellState(r, c, state)
+      })
+    })
+    console.debug('new game state', gameState.value)
+  }
+
+  const cellState = (rowIdx, colIdx, state) => {
+    const cell = state[rowIdx][colIdx]
+    const neighbors = neighborsFor(rowIdx, colIdx)
+    if (cell === A) {
+      // cell is alive
+      let numLiveNeighbors = 0
+      _.forEach(neighbors, ([r, c]) => {
+        // console.debug('row', r, 'col', c)
+        if (state[r][c] === A) {
+          numLiveNeighbors++
+        }
+        if (numLiveNeighbors > 3) {
+          return false // break
+        }
+      })
+      if (numLiveNeighbors === 2 || numLiveNeighbors === 3) {
+        return A
+      }
+
+    } else {
+      // cell is dead
+      let numLiveNeighbors = 0
+      _.forEach(neighbors, ([r, c]) => {
+        if (state[r][c] === A) {
+          numLiveNeighbors++
+        }
+        if (numLiveNeighbors > 4) {
+          return false // break
+        }
+      })
+      if (numLiveNeighbors === 3) {
+        return A
+      }
+
+    }
+
+    return D
+  }
+
+  const neighborsFor = (rowIdx, colIdx) => {
+    const n = []
+    for (let r = rowIdx - 1; r <= rowIdx + 1; r++) {
+      for (let c = colIdx - 1; c <= colIdx + 1; c++) {
+        if (r >= 0 && r < ROWS && c >= 0 && c <= COLS && !_.isEqual([r, c], [rowIdx, colIdx])) {
+          n.push([r, c])
+        }
+      }
+    }
+    return n
+  }
+
+  setInterval(iterateGame, 2000)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <table>
+    <tr 
+      v-for="rowIdx in _.range(ROWS)"
+      :key="rowIdx"
+      >
+      <td
+        v-for="colIdx in _.range(COLS)"
+        :key="colIdx"
+        >{{ gameState[rowIdx][colIdx] }}</td>
+    </tr>
+  </table>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
 import './assets/main.css'
+
+// import App from 'app'
+import App from './App.vue'
+
 
 createApp(App).mount('#app')
